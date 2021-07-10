6806489d7e47574e17cb5c4e917a3c2c7fd955ca
Jamal Hasuev <metafra@mail.ru>
2021-07-10 21:30:35 +0300

    init + add html layout

---
+++ src/App.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="app">
+    <h1 class="header">File uploader</h1>
+    
+    <div class="dropzone">
+      Перенесите файл сюда!
+    </div>
+
+    <button class="btn">Отправить</button>
+    
+    <div class="miniatures">
+      <div class="miniatures__list">
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+        <div class="miniatures__list-item">
+          <div class="miniatures__list-preview" style="background-image: url(https://picsum.photos/200/300)" />
+          <div class="miniatures__list-title">Картинка №1</div>
+        </div>
+      </div>
+      <div class="miniatures__text">Здесь будут отображаться ваши загруженные файлы.</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style scoped lang="scss">
+  .app {
+    padding: 0 15px 50px;
+    text-align: center;
+    background-color: #F4F4F6;
+    min-height: 100vh;
+  }
+
+  .header {
+    padding: 20px 0;
+    margin-bottom: 50px;
+
+    font-weight: bold;
+    font-size: 30px;
+    color: #666;
+    border-bottom: 1px solid #ddd;
+  }
+
+  .dropzone {
+    max-width: 520px;
+    min-height: 420px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+    border: 1px solid #ddd;
+    color: #999;
+
+    margin-bottom: 20px;
+  }
+
+  .btn {
+    margin-bottom: 20px;
+  }
+
+  .miniatures {
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-left: none;
+    border-right: none;
+    overflow: hidden;
+
+    &__list {
+      display: inline-flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      max-width: 600px;
+      margin: -10px;
+
+      &-item {
+        width: 150px;
+        border: 2px solid #333;
+        border-radius: 10px;
+        overflow: hidden;
+
+        margin: 10px;
+      }
+      &-preview {
+        min-height: 115px;
+        background: center center no-repeat;
+        background-size: contain;
+      }
+      &-title {
+        background-color: #333;
+        color: #ddd;
+        padding: 10px 5px;
+        font-size: 12px;
+      }
+    }
+    
+    &__text {
+      color: #777;
+    }
+  }
+</style>

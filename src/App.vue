ac8fc4698d625ab87c6f1bfa7b919707be767138
Jamal Hasuev <metafra@mail.ru>
2021-07-10 22:59:37 +0300

    fix login

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,17 @@
       @dragleave.prevent="dragLeave"
       @dragover.prevent="dragOver"
     >
-      {{ getDropzoneText }}
+      <div
+        v-if="file"
+        :style="{'background-image': getImageURL(file)}"
+        class="dropzone__preview"
+      />
+      <span v-else>
+        {{ getDropzoneText }}
+      </span>
     </div>
 
-    <button class="btn">Отправить</button>
+    <button class="btn" @click="send">Отправить</button>
     
     <div class="miniatures">
       <div v-if="files.length" class="miniatures__list">
@@ -46,6 +53,7 @@
           dragover: "Отпускайте сюда эти файлы.",
         },
         state: "drop",
+        file: null,
         files: [],
       }
     },
@@ -58,27 +66,38 @@
 
     methods: {
       selectFiles(e){
-        e.dataTransfer.files.forEach(file => {
-          if(!this.fileAlreadyUploaded(file)) {
-            const reader = new FileReader()
-            reader.onload = e => {
-              this.files.push({
-                name: file.name,
-                isImage: file.type.split('/').shift() == 'image',
-                base64: e.target.result,
-              })
+        const file = e.dataTransfer.files[0]
+
+        if(!this.fileAlreadyUploaded(file.name)) {
+          const reader = new FileReader()
+          reader.onload = e => {
+            this.file = {
+              name: file.name,
+              isImage: file.type.split('/').shift() == 'image',
+              base64: e.target.result,
             }
-            reader.readAsDataURL(file)
           }
-        })
+          reader.readAsDataURL(file)
+        }
       },
 
-      fileAlreadyUploaded(file){
-        return !file
+      fileAlreadyUploaded(name){
+        return this.files.some(file => file.name === name)
       },
 
       getImageURL(file){
         return `url(${file.isImage ? file.base64 : 'https://bit.ly/3e725Hj'})`
+      },
+
+      send(){
+        if(this.file) {
+          if (this.file.isImage) {
+            console.log(this.file.base64)
+          }
+
+          this.files.push(this.file)
+          this.file = null
+        }
       },
 
       drop(e){
@@ -120,6 +139,8 @@
   }
 
   .dropzone {
+    position: relative;
+
     max-width: 520px;
     min-height: 420px;
     margin: 0 auto;
@@ -130,7 +151,20 @@
     border: 1px solid #ddd;
     color: #999;
 
+    overflow: hidden;
     margin-bottom: 20px;
+
+    &__preview {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 1;
+
+      background: center center no-repeat;
+      background-size: contain;
+    }
   }
 
   .btn {
